1893e2305fbfd66b8b257cce716c9731fc6fe3c1
jjegarcia <jjegarcia@hotmail.com>
2021-06-17 10:48:54 +0100

    added valve request message as parameter

--- src/test/kotlin/com/jje/tank/TankParameterisedTest.kt
+++ src/test/kotlin/com/jje/tank/TankParameterisedTest.kt
@@ -1,6 +1,7 @@
 package com.jje.tank
 
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,23 +26,38 @@ class TankParameterisedTest(
         assertEquals(parameters.endState, tank.state)
     }
 
+    @Test
+    fun `after event tank send valve request `() {
+
+        val tank = Tank(state = parameters.startState, inletValve = inletValve, outletValve = outLetValve)
+
+        val valveRequest: ValveRequest = parameters.valveRequest
+
+        parameters.action(tank)
+        verify(exactly = valveRequest.inletValveOpen) { inletValve.open() }
+        verify(exactly = valveRequest.inletValveClosed) { inletValve.close() }
+        verify(exactly = valveRequest.outletValveOpen) { outLetValve.open() }
+        verify(exactly = valveRequest.outletValveClosed) { outLetValve.close() }
+    }
+
     companion object {
         @JvmStatic
         @Parameterized.Parameters(name = "")
         fun getParameters(): List<TestParameters> {
             return listOf(
-                    TestParameters(TankState.FILLING, Tank::flush, TankState.FILLING),
-                    TestParameters(TankState.FILLING, Tank::lowLevel, TankState.FILLING),
-                    TestParameters(TankState.FILLING, Tank::highLevel, TankState.FULL),
-                    TestParameters(TankState.FULL, Tank::flush, TankState.FLUSHING),
-                    TestParameters(TankState.FULL, Tank::lowLevel, TankState.FULL),
-                    TestParameters(TankState.FULL, Tank::highLevel, TankState.FULL),
-                    TestParameters(TankState.FLUSHING, Tank::flush, TankState.FLUSHING),
-                    TestParameters(TankState.FLUSHING, Tank::lowLevel, TankState.FILLING),
-                    TestParameters(TankState.FLUSHING, Tank::highLevel, TankState.FLUSHING)
+                    TestParameters(TankState.FILLING, Tank::flush, TankState.FILLING, ValveRequest(0, 0, 0, 0)),
+                    TestParameters(TankState.FILLING, Tank::lowLevel, TankState.FILLING, ValveRequest(0, 0, 0, 0)),
+                    TestParameters(TankState.FILLING, Tank::highLevel, TankState.FULL, ValveRequest(0, 1, 0, 0)),
+                    TestParameters(TankState.FULL, Tank::flush, TankState.FLUSHING, ValveRequest(0, 0, 1, 0)),
+                    TestParameters(TankState.FULL, Tank::lowLevel, TankState.FULL, ValveRequest(0, 0, 0, 0)),
+                    TestParameters(TankState.FULL, Tank::highLevel, TankState.FULL, ValveRequest(0, 0, 0, 0)),
+                    TestParameters(TankState.FLUSHING, Tank::flush, TankState.FLUSHING, ValveRequest(0, 0, 0, 0)),
+                    TestParameters(TankState.FLUSHING, Tank::lowLevel, TankState.FILLING, ValveRequest(1, 0, 0, 1)),
+                    TestParameters(TankState.FLUSHING, Tank::highLevel, TankState.FLUSHING, ValveRequest(0, 0, 0, 0))
             )
         }
 
-        data class TestParameters(val startState: TankState, val action: (Tank) -> Unit, val endState: TankState)
+        data class TestParameters(val startState: TankState, val action: (Tank) -> Unit, val endState: TankState, val valveRequest: ValveRequest)
+        data class ValveRequest(val inletValveOpen: Int, val inletValveClosed: Int, val outletValveOpen: Int, val outletValveClosed: Int)
     }
 }
