f442affe898341c809c5314372619c79f1a8f0a6
jjegarcia <jjegarcia@hotmail.com>
2021-06-17 08:23:41 +0100

    simplified parametrised tests

--- src/test/kotlin/com/jje/tank/TankParameterisedTest.kt
+++ src/test/kotlin/com/jje/tank/TankParameterisedTest.kt
@@ -9,7 +9,9 @@ import kotlin.reflect.KFunction1
 
 
 @RunWith(value = Parameterized::class)
-class TankParameterisedTest(val startState: TankState, val action: (Tank) -> Unit, val endState: TankState) {
+class TankParameterisedTest(
+        private val parameters: TestParameters
+) {
 
     private val inletValve: Valve = mockk<Valve>(relaxed = true)
     private val outLetValve = mockk<Valve>(relaxed = true)
@@ -17,21 +19,22 @@ class TankParameterisedTest(val startState: TankState, val action: (Tank) -> Uni
     @Test
     fun `after event tank ends in correct state`() {
 
-        val tank = Tank(state = startState, inletValve = inletValve, outletValve = outLetValve)
+        val tank = Tank(state = parameters.startState, inletValve = inletValve, outletValve = outLetValve)
 
-        action(tank)
-        assertEquals(endState, tank.state)
+        parameters.action(tank)
+        assertEquals(parameters.endState, tank.state)
     }
 
     companion object {
-        data class TestParameters(val startState: TankState, val method: (Tank) -> Unit, val endState: TankState) {
-            val array: Array<Any>
-                get() = arrayOf(startState, method, endState)
+        @JvmStatic
+        @Parameterized.Parameters(name = "")
+        fun getParameters(): List<TestParameters> {
+            return listOf(
+                    TestParameters(TankState.FULL, Tank::flush, TankState.FLUSHING),
+                    TestParameters(TankState.FULL, Tank::flush, TankState.FLUSHING)
+            )
         }
 
-        @get:Parameterized.Parameters
-        @JvmStatic
-        val parameters: List<Array<Any>>
-            get() = listOf(TestParameters(TankState.FULL, Tank::flush, TankState.FLUSHING).array)
+        data class TestParameters(val startState: TankState, val action: (Tank) -> Unit, val endState: TankState)
     }
 }
