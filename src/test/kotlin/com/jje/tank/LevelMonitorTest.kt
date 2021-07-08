2c47981ad2ff43c3a5b75b1c8118f00c805163e8
jjegarcia <jjegarcia@hotmail.com>
2021-07-08 13:01:22 +0100

    introducing LevelMonitor.kt todo: add do nothing conditions and then edge cases

---
+++ src/test/kotlin/com/jje/tank/LevelMonitorTest.kt
@@ -0,0 +1,58 @@
+package com.jje.tank
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+
+class LevelMonitorTest {
+
+    @Test
+    fun `given Flushing when transition above to below low level then Low Level`() {
+
+        val tank = mockk<Tank>(relaxed = true)
+        every { tank.state } returns TankState.FLUSHING
+        val levelMonitor = LevelMonitor(tank)
+        levelMonitor.current(LOW_LEVEL+1)
+        levelMonitor.current(LOW_LEVEL)
+
+
+        verify {
+            tank.lowLevel()
+        }
+    }
+
+    @Test
+    fun `given Flushing when transition not met then nothing happens`() {
+
+        val tank = mockk<Tank>(relaxed = true)
+        every { tank.state } returns TankState.FLUSHING
+        val levelMonitor = LevelMonitor(tank)
+        levelMonitor.current(LOW_LEVEL+2)
+        levelMonitor.current(LOW_LEVEL+1)
+
+
+        verify(exactly = 0) {
+            tank.lowLevel()
+        }
+    }
+
+    @Test
+    fun `given Filling when transition below to high level then high level`() {
+
+        val tank = mockk<Tank>(relaxed = true)
+        every { tank.state } returns TankState.FILLING
+        val levelMonitor = LevelMonitor(tank)
+        levelMonitor.current(HIGH_LEVEL-1)
+        levelMonitor.current(HIGH_LEVEL)
+
+
+        verify {
+            tank.highLevel()
+        }
+    }
+
+
+
+}
