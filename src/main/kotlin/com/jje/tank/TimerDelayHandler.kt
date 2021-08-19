74eee21dd79cde038e6953bd96aad196b76c875e
Jenaro Garcia <jjegarcia@hotmail.com>
2021-08-19 10:35:43 +0100

    replace delay with Thread.slee

--- src/main/kotlin/com/jje/tank/TimerDelayHandler.kt
+++ src/main/kotlin/com/jje/tank/TimerDelayHandler.kt
@@ -1,25 +1,12 @@
 package com.jje.tank
 
-import java.util.*
-import kotlin.concurrent.schedule
-
 interface TimerHandler {
-    fun setTimer(miliseconds: Long)
     fun delay(miliseconds: Long)
 }
 
 class TimerDelayHandler : TimerHandler {
-    var completed: Boolean = false
-    override fun setTimer(miliseconds: Long) {
-        Timer("SettingUp", false).schedule(miliseconds) {
-            completed = true
-        }
-    }
 
     override fun delay(miliseconds: Long) {
-        completed = false
-        while (completed != true) {
-            setTimer(miliseconds = miliseconds)
-        }
+        Thread.sleep(miliseconds)
     }
 }
